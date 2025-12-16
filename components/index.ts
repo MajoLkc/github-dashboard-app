a896b5ae0183a98adccb1af779040ed7ee3e031b
MajoLkc <lukac.1majo@gmail.com>
2025-12-16 14:34:23 +0100

    Export Toast and LoginModal components in index file

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,6 @@ export { Header } from "./Header";
 export { SearchBar } from "./SearchBar";
 export { UserProfile } from "./UserProfile";
 export { ErrorState } from "./ErrorState";
+export { Toast } from "./Toast";
+export { LoginModal } from "./LoginModal";
 export * from "./Icons";
