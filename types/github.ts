32f955fc4119dfd6b3cd70b8e7cbb19508081661
MajoLkc <lukac.1majo@gmail.com>
2025-12-16 07:41:38 +0100

    Add GitHubUser and GitHubError interfaces for type definitions

---
+++ types/github.ts
@@ -0,0 +1,22 @@
+export interface GitHubUser {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  name: string | null;
+  company: string | null;
+  blog: string | null;
+  location: string | null;
+  email: string | null;
+  bio: string | null;
+  twitter_username: string | null;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+}
+
+export interface GitHubError {
+  message: string;
+  documentation_url?: string;
+}
