b81f8c79228e82899274f8f9ee35b6ef5290a17c
MajoLkc <lukac.1majo@gmail.com>
2025-12-16 07:41:25 +0100

    Add index file to export components for easier imports

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export { ThemeProvider, useTheme } from "./ThemeProvider";
+export { Header } from "./Header";
+export { SearchBar } from "./SearchBar";
+export { UserProfile } from "./UserProfile";
+export { ErrorState } from "./ErrorState";
+export * from "./Icons";
